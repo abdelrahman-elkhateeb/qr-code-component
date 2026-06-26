2c87e5225230733e50a5a575a9ccf9804a5d049a
Abdelrahman Elkhateeb <109158103+abdelrahman-elkhateeb@users.noreply.github.com>
2026-06-26 19:00:47 +0300

    Add QR card UI, fonts and assets
    Implement a centered QR-code card UI in App.tsx (renders imported QR image and text). Add Google 'Outfit' font links to index.html and define theme variables and font usage in App.css. Add favicon and QR image assets, remove unused react.svg and vite.svg, and drop the index.css import from main.tsx.

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import './App.css'
+import img from "../src/assets/image-qr-code.png";
 
 function App() {
 
   return (
-    <>
-      <h1>hi</h1>
-    </>
+    <main className='h-dvh bg-slate-300 grid place-content-center'>
+      <div className='w-72 bg-white text-center p-3 rounded-2xl'>
+        <div>
+          <img
+            src={img}
+            alt="QR"
+            className="w-full rounded-lg"
+          />
+        </div>
+        <h1 className='text-slate-900 text-[22px] leading-[1.2] font-extrabold mt-4'>
+          Improve your front-end skills by building projects
+        </h1>
+        <p className='text-slate-500 text-body leading-[1.4] tracking-[0.2] mt-3 mb-5'>scan the QR code visit Frontend Mentor and take your coding skills to the next level</p>
+      </div>
+    </main>
   )
 }
 
-export default App
+export default App;
